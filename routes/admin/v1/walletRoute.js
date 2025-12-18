0939d039451d9b8d6341b4288bc958b56c256113
APPLE <apple@APPLEs-MacBook-Pro.local>
2025-12-18 11:30:49 +0530

    small changes

--- routes/admin/v1/walletRoute.js
+++ routes/admin/v1/walletRoute.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const walletController = require('../../../controller/admin/v1/walletContoller');
+const authentication = require('../../../middleware/authentication');
 
-router.post('/alsWallet', walletController.alsWallet);
+router.post('/alsWallet', authentication, walletController.alsWallet);
 
 module.exports = router;
