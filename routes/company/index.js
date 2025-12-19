eeb6f8048f42747c7127ab26b32b016be1ed8ad9
APPLE <apple@APPLEs-MacBook-Pro.local>
2025-12-19 17:15:51 +0530

    small changes

--- routes/company/index.js
+++ routes/company/index.js
@@ -4,12 +4,9 @@ const authentication = require('../../middleware/authentication');
 const { route } = require('../admin');
 
 router.use('/companyDetails', require('./v1/companyRoutes'));
-// Image routes
 router.use('/images',authentication, require('./v1/imageRoutes'));
-
 router.use('/onboarding', require('./v1/onboardingRoute'));
-
-// User management routes (upgrade/degrade)
 router.use('/user', authentication, require('./v1/userRoutes'));
+router.use('/reports', authentication, require('./v1/reportsRoutes'));
 
 module.exports = router;
