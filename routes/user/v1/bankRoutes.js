6022d507fc9d3aff0cd9baa2b980c8081884f98f
APPLE <apple@APPLEs-MacBook-Pro.local>
2025-12-19 14:07:50 +0530

    small changes

--- routes/user/v1/bankRoutes.js
+++ routes/user/v1/bankRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bankController = require('../../../controller/user/v1/bankController');
+const bankController = require('../../../controller/admin/v1/bankController');
 const authentication = require('../../../middleware/authentication');
 
 router.post('/get-all-banks', authentication, bankController.getAllBanks);
