ce16e6197b655c7e08fe155dc7e89caa4f6d97f8
APPLE <apple@APPLEs-MacBook-Pro.local>
2025-12-31 13:14:11 +0530

    small changes

--- index.js
+++ index.js
@@ -190,19 +190,16 @@ function name() {
 }
 
 if (process.env.NODE_ENV !== 'test') {
-  models.sequelize
-    .sync({ alter: true })
-    .then(() => {})
-    .catch((error) => {
-      console.error('Database sync error:', error.message);
-    })
-    .finally(() => {
-      app.use(routes);
-      // seeder();
-      name();
-      aepsLogout();
-    });
-  // app.use(routes);
+  // models.sequelize
+  //   .sync({ alter: true })
+  //   .then(() => {})
+  //   .finally(() => {
+  //     app.use(routes);
+  //     // seeder();
+  //     name();
+  //     aepsLogout();
+  //   });
+  app.use(routes);
   httpServer.listen(process.env.PORT, () => {
     console.log(`gmaxepay is running on port ${process.env.PORT} successfully.`);
   });
