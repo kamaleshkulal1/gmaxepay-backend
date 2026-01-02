f8be4fe7a1a0e76412ed16712de2a66214311518
APPLE <apple@APPLEs-MacBook-Pro.local>
2026-01-02 11:38:27 +0530

    payout logic

--- routes/user/v1/payoutRoute.js
+++ routes/user/v1/payoutRoute.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const payoutController = require('../../../controller/user/v1/payoutController');
 const authentication = require('../../../middleware/authentication');
 
+// Process payout
+router.post('/', authentication, payoutController.payout);
+
 // Get payout bank list
 router.post('/bank-list', authentication, payoutController.getPayoutBankList);
-
-// Process payout
-router.post('/payout', authentication, payoutController.payout);
 
 module.exports = router;
