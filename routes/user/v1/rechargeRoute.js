e8e94a1eb7206b7cdfe981c8ddddba8917ad4fe8
APPLE <apple@APPLEs-MacBook-Pro.local>
2026-01-15 14:54:50 +0530

    deploment

--- routes/user/v1/rechargeRoute.js
+++ routes/user/v1/rechargeRoute.js
@@ -5,7 +5,6 @@ const authentication = require('../../../middleware/authentication');
 
 router.post('/find-mobile-operator', authentication, rechargeController.findMobileNumberOperator);
 router.post('/pay', authentication, rechargeController.recharge);
-router.post('/recent-recharage', authentication, rechargeController.rechargeWithWallet);
 router.post('/find-recharge-plan',authentication, rechargeController.findAllRechargePlanFetch);
 router.post('/recharge-offer',authentication, rechargeController.findRechargeOfferFetch);
 router.post('/get-recharge-history', authentication, rechargeController.getRechargeHistory);
