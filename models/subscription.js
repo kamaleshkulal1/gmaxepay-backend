ac1e31abe2a1cac6b7a5c0091a40de39d8fbeefc
APPLE <apple@APPLEs-MacBook-Pro.local>
2026-02-03 10:24:43 +0530

    DB Syc

--- models/subscription.js
+++ models/subscription.js
@@ -17,7 +17,7 @@ let Subscription = sequelize.define(
       type: DataTypes.INTEGER,
       allowNull: false,
       references: {
-        model: 'slabs',
+        model: 'slab',
         key: 'id'
       }
     },
