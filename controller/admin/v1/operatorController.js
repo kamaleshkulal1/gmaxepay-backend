d9a27d89c59beba9608fb0fcfc174683ea456235
APPLE <apple@APPLEs-MacBook-Pro.local>
2026-02-05 23:41:24 +0530

    chnages in operator

--- controller/admin/v1/operatorController.js
+++ controller/admin/v1/operatorController.js
@@ -81,6 +81,7 @@ const registerOperator = async (req, res) => {
       const addedByRole = slabData.addedByRole;
       const slabCompanyId = slabData.companyId;
       const addedBy = slabData.addedBy;
+      console.log('addedBy', addedBy);
 
       const config = getRoleConfig(addedByRole);
       const roleTypes = config.roleTypes;
