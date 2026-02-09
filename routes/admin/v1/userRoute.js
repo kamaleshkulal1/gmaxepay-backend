3411a593cf5ba71959569a47ea0ad25a93e4f6ef
APPLE <apple@APPLEs-MacBook-Pro.local>
2026-02-09 15:21:47 +0530

    small change

--- routes/admin/v1/userRoute.js
+++ routes/admin/v1/userRoute.js
@@ -5,6 +5,10 @@ const authentication = require('../../../middleware/authentication');
 
 router.post('/create', userController.createUser);
 router.post('/list', authentication, userController.findAllUsers);
+router.post('/getProfile', authentication, userController.getProfile);
+router.post('/company-admin/:id', authentication, userController.getCompanyAdminById);
+router.post('/bank-details/upload', authentication, userController.uploadBankDetailsForUser);
+router.post('/profile/:id', authentication, userController.getByUserProfile);
 router.post('/:id', authentication, userController.getUser);
 router.put('/:id', authentication, userController.updateUser);
 router.delete('/:id', authentication, userController.deleteUser);
@@ -12,9 +16,4 @@ router.post('/:id/unlock', authentication, userController.unlockAccount);
 router.post('/:id/kyc/status', authentication, userController.getKycVerificationStatus);
 router.post('/:id/kyc/complete', authentication, userController.getCompleteKycData);
 router.post('/:id/kyc/revert', authentication, userController.revertKycData);
-router.post('/company-admin/:id', authentication, userController.getCompanyAdminById);
-router.post('/bank-details/upload', authentication, userController.uploadBankDetailsForUser);
-router.post('/profile/:id', authentication, userController.getByUserProfile);
-router.post('/getProfile', authentication, userController.getProfile);
-
 module.exports = router;
