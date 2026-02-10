d9566b012e7e73caec87a5a71287c8d796d371f2
APPLE <apple@APPLEs-MacBook-Pro.local>
2026-02-11 01:17:57 +0530

    CompanyDashboard

--- routes/user/v1/dashboardRoute.js
+++ routes/user/v1/dashboardRoute.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const authentication = require('../../../middleware/authentication');
 const dashboardController = require('../../../controller/user/v1/dashboardController');
 
-router.post('/statistics', dashboardController.getDashboard);
+
+router.post('/statistics', authentication, dashboardController.getDashboard);
 
 module.exports = router;
