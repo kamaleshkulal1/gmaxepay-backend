63f924ce0fb10e3c94e2878ac1989cca009e5526
APPLE <apple@APPLEs-MacBook-Pro.local>
2026-02-10 19:01:55 +0530

    Dthcorrections

--- routes/company/v1/payoutRoutes.js
+++ routes/company/v1/payoutRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const payoutController = require('../../../controller/company/v1/payoutController');
 const authentication = require('../../../middleware/authentication');
 
-router.post('/payout', authentication, payoutController.payout);
+router.post('/', authentication, payoutController.payout);
 router.post('/bank-list', authentication, payoutController.getPayoutBankList);
 
 module.exports = router;
