9cce9bee68510825f5dd39d21c5b09af27a0c754
APPLE <apple@APPLEs-MacBook-Pro.local>
2026-02-19 18:41:18 +0530

    add bank

--- services/razorpayApi.js
+++ services/razorpayApi.js
@@ -1,15 +1,17 @@
 const axios = require('axios');
 const razorpayUrl = process.env.RAZORPAY_IFSC_URL;
 const bankDetails = async (ifsc) => {
-    try{
+    try {
         const response = await axios.get(`${razorpayUrl}/${ifsc.trim()}`);
+        console.log("reponse ifsc Code", ifsc);
+        console.log(response.data);
         return response.data;
-    }catch(error){
-        console.error(error);
-        return error.response.data;
+    } catch (error) {
+        console.error(error); // Suppress verbose error logging for 404s
+        return null;
     }
 }
 
 module.exports = {
-  bankDetails
+    bankDetails
 }
