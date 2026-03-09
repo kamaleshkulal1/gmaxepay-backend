38fa0c9af5f6319f07799489267dc888fee04ee3
APPLE <apple@APPLEs-MacBook-Pro.local>
2026-03-09 19:07:38 +0530

    small chnages

--- routes/admin/v1/walletRoute.js
+++ routes/admin/v1/walletRoute.js
@@ -8,5 +8,6 @@ router.post('/balance', authentication, walletController.walletBalance);
 router.post('/inspayWallet', authentication, walletController.inspayWallet);
 router.post('/bbpsWallet', authentication, walletController.bbpsWallet);
 router.post('/walletHistory', authentication, walletController.walletHistory);
+router.post('/a1TopupWallet', authentication, walletController.a1TopupWallet);
 
 module.exports = router;
