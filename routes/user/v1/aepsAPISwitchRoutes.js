c1c9d3caa8183725b096c57210eca62fd61768dd
APPLE <apple@APPLEs-MacBook-Pro.local>
2026-05-11 17:47:44 +0530

    payout ASL

--- routes/user/v1/aepsAPISwitchRoutes.js
+++ routes/user/v1/aepsAPISwitchRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const aepsAPISwitchController = require('../../../controller/user/v1/aepsAPISwitchController');
 const authentication = require('../../../middleware/authentication');
 
-router.get('/active', authentication, aepsAPISwitchController.getActiveAepsAPI);
+router.get('/', authentication, aepsAPISwitchController.getActiveAepsAPI);
 
 module.exports = router;
