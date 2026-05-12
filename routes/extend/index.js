ff4b81f46743c128621fa976849a3806767f93aa
APPLE <apple@APPLEs-MacBook-Pro.local>
2026-05-12 17:29:50 +0530

    asl wallet update

--- routes/extend/index.js
+++ routes/extend/index.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { createVendorPayout, vendorWalletBalance } = require('../../controller/extend/v1/vendorPayout');
 
 router.post('/vendor-payout', createVendorPayout);
-router.post('/vendor-wallet-balance', vendorWalletBalance);
+router.post('/vendor-balance', vendorWalletBalance);
 
 module.exports = router;
