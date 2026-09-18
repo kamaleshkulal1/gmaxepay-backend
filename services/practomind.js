778d3f726d0e8b47af8913442c81bac2d492dfda
APPLE <kamalesh.kulal@gmaxepay.in>
2026-09-18 14:03:17 +0530

    aeps update

--- services/practomind.js
+++ services/practomind.js
@@ -141,8 +141,7 @@ const practomindAepsOnboarding = async (data, merchantLoginId) => {
 
     const headers = await getHeaders(payload);
     const response = await axios.post(`${BASE_URL}/api/partner/v1/aeps/merchant/register`, payload, { headers });
-    console.log("Practomind Response", JSON.stringify(response, null, 2));
-    console.log('Practomind Merchant Register Response:', response.data);
+    console.log("Practomind Response:", JSON.stringify(response.data, null, 2));
     return response.data;
   } catch (error) {
     console.error('Practomind Merchant Register Error:', error.response?.data || error.message);
