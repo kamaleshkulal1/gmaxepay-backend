0273e0aee3b4b5983aec8e7761092b31d23b427f
APPLE <kamalesh.kulal@gmaxepay.in>
2026-09-18 11:20:11 +0530

    aeps update

--- services/practomind.js
+++ services/practomind.js
@@ -104,7 +104,7 @@ const sanitizeLogPayload = (payload) => {
 const practomindAepsOnboarding = async (data, merchantLoginId) => {
   try {
     const payload = {
-      merchantRefId: String(data.merchantRefId || merchantLoginId || data.merchantLoginId || data.merchantPhoneNumber || ''),
+      merchantRefId: String('GMAX000093'),
       firstName: data.firstName || data.merchantFirstName || '',
       lastName: data.lastName || data.merchantLastName || '',
       middleName: data.middleName || data.merchantMiddleName || '',
